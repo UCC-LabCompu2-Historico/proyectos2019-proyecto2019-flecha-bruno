b12fa8a531fb8f1f6bd00e3010c70d883d35f08a
augusto2902 <augustobruno2902@gmail.com>
2019-06-21 21:55:29 -0300

    21 4

---
+++ javascript/ECU1.js
@@ -0,0 +1,16 @@
+function resolver1() {
+    var resultado;
+    var a= parseFloat(document.getElementById("num1").value);
+    var b= parseFloat(document.getElementById("num2").value);
+    var c= parseFloat(document.getElementById("num3").value);
+    var d= parseFloat(document.getElementById("num4").value);
+    if(c===0&&a===0){document.write("no existe x ")}
+    else if(a===c){document.write("no existe x ")}
+    else if(a===1&&c===0&&d===0){resultado=(-1*b);document.write("x es ="+ resultado)}
+    else if(a===0&&b===0&&c===1){resultado=(-1*d);document.write("x es ="+ resultado)}
+    else if(a===0&&b===0&&c!==1){resultado=((-1*d)/c);document.write("x es ="+ resultado)}
+    else if(d===0&&c===0&&a!==1){resultado=((-1*b)/a);document.write("x es ="+ resultado)}
+    else if(c!==0&&a!==0){resultado=(((-1*b)+d)/(a+(-1*c)));document.write("x es ="+ resultado)}
+    else {resultado=(((-1*b)+d)/(a+(-1*c)));document.write("x es ="+ resultado)}
+
+}
