676c5859bb2df4d4e4ad5b38bd636dce309df814
Facundo Flecha <flechafacundo@gmail.com>
2019-06-21 22:22:04 -0300

    2.6

--- javascript/ECU2.js
+++ javascript/ECU2.js
@@ -5,7 +5,8 @@ var c= parseFloat(document.getElementById("num3").value);
 if (((b*b)+(-4*a*c))>=0) {
     solution1 = ((-1 * b) + ((Math.sqrt(Math.pow(b, 2) - (4 * a * c))))) / (2 * a);
     solution2 = ((-1 * b) - ((Math.sqrt(Math.pow(b, 2) - (4 * a * c))))) / (2 * a);
-    document.write("la primer raiz es ="+ solution1+ "</br>");
-    document.write("la segunda raiz es ="+ solution2);
+    document.write("La primer raiz es ="+ solution1+ "</br>");
+    document.write("La segunda raiz es ="+ solution2);
 }
-else{document.write("la raiz es imaginaria")}}
+else{document.write("La raiz es imaginaria")}}
+
