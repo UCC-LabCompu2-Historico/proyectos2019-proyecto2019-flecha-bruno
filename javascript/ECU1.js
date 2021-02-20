cd7ae155d74798182e17c2a7e840d02931495404
augusto2902 <augustobruno2902@gmail.com>
2021-02-20 17:58:27 -0300

    js comentado

--- javascript/ECU1.js
+++ javascript/ECU1.js
@@ -1,14 +1,14 @@
 function resolver1() {
     var resultado;var a,b,c,d;
-    var a= parseFloat(document.getElementById("num1").value);
+    var a= parseFloat(document.getElementById("num1").value);/*toma valor introducido en html y lo guarda en variable*/
     var b= parseFloat(document.getElementById("num2").value);
     var c= parseFloat(document.getElementById("num3").value);
     var d= parseFloat(document.getElementById("num4").value);
-    if(isNaN(a)) a=0;
+    if(isNaN(a)) a=0;/*controla que el valor no sea Nan y si lo es lo cambia por 0*/
     if(isNaN(b)) b=0;
     if(isNaN(c)) c=0;
     if(isNaN(d)) d=0;
-    if(c===0&&a===0){document.write("No existe x ")}
+    if(c===0&&a===0){document.write("No existe x ")}/*condiciones para distintos tipos de resultados en la ecuacion*/
     else if(a===c){document.write("No existe x ")}
     else if(a===1&&c===0&&d===0){resultado=(-1*b);document.write("x es ="+ resultado)}
     else if(a===0&&b===0&&c===1){resultado=(-1*d);document.write("x es ="+ resultado)}
--- javascript/ECU2.js
+++ javascript/ECU2.js
@@ -1,13 +1,13 @@
 function resolver() {var solution1,solution2;
-var a= parseFloat(document.getElementById("num1").value);
+var a= parseFloat(document.getElementById("num1").value);/*toma valor introducido en html y lo guarda en variable*/
 var b= parseFloat(document.getElementById("num2").value);
 var c= parseFloat(document.getElementById("num3").value);
-    if(isNaN(a)) a=0;
+    if(isNaN(a)) a=0;/*controla que el valor no sea Nan y si lo es lo cambia por 0*/
     if(isNaN(b)) b=0;
     if(isNaN(c)) c=0;
-    if(a===0&&b===0){document.write("no hay raices");}
-    if(a===0){document.write("es una raiz de primer grado use el otro metodo");}
-    else if (((b*b)+(-4*a*c))>=0) {
+    if(a===0&&b===0){document.write("no hay raices");}/*controla si la raiz existe*/
+    if(a===0){document.write("es una raiz de primer grado use el otro metodo");}/*controla si el metodo es correcto*/
+    else if (((b*b)+(-4*a*c))>=0) {/*resuelve la ecuacion de segundo grado*/
     solution1 = ((-1 * b) + ((Math.sqrt(Math.pow(b, 2) - (4 * a * c))))) / (2 * a);
     solution2 = ((-1 * b) - ((Math.sqrt(Math.pow(b, 2) - (4 * a * c))))) / (2 * a);
     document.write("La primer raiz es ="+ solution1+ "</br>");
